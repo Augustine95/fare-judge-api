0d653648ad351e72683fcd8e29ca132726547c52
Augustine Awuori <augutineawuroi95@gmail.com>
2022-04-05 13:51:32 +0300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,7 @@
+const winston = require('winston');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
