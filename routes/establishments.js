6c2c56ed89373a0978b0c5e653a017fa93c2a6bb
Augustine Awuori <augutineawuroi95@gmail.com>
2022-04-06 13:52:23 +0300

    Write establishment test.

--- routes/establishments.js
+++ routes/establishments.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const validateObjectId = require("../middleware/validateObjectId");
 const router = express.Router();
-const { Establishment } = require('../models/establishment');
+const { Establishment, validate } = require('../models/establishment');
+const validator = require('../middleware/validate');
 
 router.get('/', async (req, res) => {
     const establishments = await Establishment.find({});
@@ -13,6 +15,37 @@ router.get('/:id', validateObjectId, async (req, res) => {
 
     if (!establishment)
         return res.status(404).send("The establishment with the given ID was found.");
+
+    res.send(establishment);
+});
+
+router.post('/', [auth, validator(validate)], async (req, res) => {
+    const establishment = new Establishment({
+        name: req.body.name,
+        image: req.body.image,
+        location: req.body.location
+    });
+    await establishment.save();
+
+    res.send(establishment);
+});
+
+router.put('/:id', [auth, validateObjectId, validator(validate)], async (req, res) => {
+    const establishment = await Establishment.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        image: req.body.image,
+        location: req.body.location
+    }, { new: true });
+
+    if (!establishment) return res.status(404).send('The establishment with the given ID was not found.');
+
+    res.send(establishment);
+});
+
+router.delete('/:id', [auth, validateObjectId], async (req, res) => {
+    const establishment = await Establishment.findByIdAndRemove(req.params.id);
+
+    if (!establishment) return res.status(404).send('The establishment with the given ID was not found.');
 
     res.send(establishment);
 });
--- tests/integration/establishment.test.js
+++ tests/integration/establishment.test.js
@@ -1,73 +1,143 @@
 const request = require("supertest");
 const { Establishment } = require("../../models/establishment");
+const { User } = require("../../models/user");
 const mongoose = require("mongoose");
 
 describe("/api/establishments", () => {
-    let server;
+  let server;
 
-    beforeEach(() => {
-        server = require("../../server");
+  beforeEach(() => {
+    server = require("../../server");
+  });
+
+  afterEach(async () => {
+    await Establishment.deleteMany({});
+    await server.close();
+  });
+
+  describe("GET /", () => {
+    const exec = () => {
+      return request(server).get("/api/establishments/");
+    };
+
+    it("should return all establishments", async () => {
+      const establishments = [
+        { name: "establishment1", image: "12345", location: "12345" },
+        { name: "establishment2", image: "12345", location: "12345" },
+      ];
+      await Establishment.collection.insertMany(establishments);
+
+      const res = await exec();
+
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((e) => e.name === "establishment1")).toBeTruthy();
+      expect(res.body.some((e) => e.name === "establishment2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    let id;
+
+    const exec = () => request(server).get("/api/establishments/" + id);
+
+    it("should return an establishment if the id is valid", async () => {
+      const establishment = new Establishment({
+        name: "establishment1",
+        image: "12345",
+        location: "12345",
+      });
+      await establishment.save();
+
+      id = establishment._id;
+      const res = await exec();
+
+      expect(res.status).toBe(200);
+    });
+
+    it("should return 400 if  invalid id is passed", async () => {
+      id = 1;
+
+      const res = await exec();
+
+      expect(res.status).toBe(404);
+    });
+
+    it("should return 404 if no establishment with the given ID exist.", async () => {
+      id = mongoose.Types.ObjectId();
+
+      const res = await exec();
+
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe("POST /", () => {
+    let establishment;
+    let token;
+
+    beforeEach(async () => {
+      establishment = new Establishment({
+        name: "establishment1",
+        image: "12345",
+        location: "12345",
+      });
+      await establishment.save();
+      token = new User().generateAuthToken();
     });
 
     afterEach(async () => {
-        await Establishment.deleteMany({});
-        await server.close();
+      await User.deleteMany({});
+      await Establishment.deleteMany({});
     });
 
-    describe("GET /", () => {
-        const exec = () => {
-            return request(server).get("/api/establishments/");
-        };
+    const exec = () => {
+      return request(server)
+        .post("/api/establishments")
+        .set("x-auth-token", token)
+        .send({ establishment });
+    };
 
-        it("should return all establishments", async () => {
-            const establishments = [
-                { name: "establishment1", image: "12345", location: "12345" },
-                { name: "establishment2", image: "12345", location: "12345" },
-            ];
-            await Establishment.collection.insertMany(establishments);
+    it("should return 401 if client is not logged in", async () => {
+      token = "";
 
-            const res = await exec();
+      const res = await exec();
 
-            expect(res.status).toBe(200);
-            expect(res.body.length).toBe(2);
-            expect(res.body.some((e) => e.name === "establishment1")).toBeTruthy();
-            expect(res.body.some((e) => e.name === "establishment2")).toBeTruthy();
-        });
+      expect(res.status).toBe(401);
     });
 
-    describe("GET /:id", () => {
-        let id;
+    it("should return 400 if token is invalid", async () => {
+      token = "a";
 
-        const exec = () => request(server).get("/api/establishments/" + id);
+      const res = await exec();
 
-        it("should return an establishment if the id is valid", async () => {
-            const establishment = new Establishment({
-                name: "establishment1",
-                image: "12345",
-                location: "12345",
-            });
-            await establishment.save();
-
-            id = establishment._id;
-            const res = await exec();
-
-            expect(res.status).toBe(200);
-        });
-
-        it("should return 400 if  invalid id is passed", async () => {
-            id = 1;
-
-            const res = await exec();
-
-            expect(res.status).toBe(404);
-        });
-
-        it("should return 404 if no establishment with the given ID exist.", async () => {
-            id = mongoose.Types.ObjectId();
-
-            const res = await exec();
-
-            expect(res.status).toBe(404);
-        });
+      expect(res.status).toBe(400);
     });
+
+    it("should return 400 if input is invalid", async () => {
+      establishment = "";
+
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    // it("should save the establishment if is valid", async () => {
+    //   await exec();
+
+    //   const result = await Establishment.findById(establishment._id);
+
+    //   expect(result).not.toBeNull();
+    //   expect(result).toHaveProperty("_id");
+    //   expect(result).toHaveProperty("name", establishment.name);
+    //   expect(result).toHaveProperty("location", establishment.location);
+    //   expect(result).toHaveProperty("image", establishment.image);
+    // });
+
+    it("should return the establishment if is valid", async () => {
+      const res = await exec();
+
+      expect(res.status).toBe(200);
+    });
+  });
 });
